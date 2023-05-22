0c55caed7616137c900d109790c722ea32f1e543
vasga-floze <gabriela.floze@gmail.com>
2023-05-22 11:28:37 -0600

    api v2 finalizada

--- app/Http/Controllers/API/v2/PostController.php
+++ app/Http/Controllers/API/v2/PostController.php
@@ -18,6 +18,7 @@ class PostController extends Controller
     public function index()
     {
         //
+        return new PostCollection(Post::latest()->paginate());
     }
 
     /**
--- app/Http/Resources/v2/PostCollection.php
+++ app/Http/Resources/v2/PostCollection.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PostCollection extends ResourceCollection
 {
+    //agregar una variable publica para que los recursos
+    //sean mostrados como recursos individuales
+    public $collects = PostResource::class;
     /**
      * Transform the resource collection into an array.
      *
@@ -14,6 +17,14 @@ class PostCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'UGB',
+                'author' => 'Vasti Gabriela Flores'
+            ],
+            'type' => 'articles'
+        ];
     }
 }
